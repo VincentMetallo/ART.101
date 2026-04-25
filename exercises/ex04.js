784668334e1aa38cb7281e923a855d2d4eb0b8a1
vmetallo-ctrl <vmetallo@ucsc.edu>
2026-04-24 23:32:50 -0700

    new

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,5 +1,19 @@
 let count = 0;
+let faces=[":3",":p",":O",";D",";0",";3",";p",":|",":3c"]
+
 $("#needy-button").click( function() {
+
+   
+
+   $("#needy-button").html("You clicked me " + count + " times" + " " + faces[count]);
+-
+-
+-
+
+   $("#needy-button").css("background-color", colors [count]);
+-
+-
+-
+
    count = count + 1;
-   $("#needy-button").html("You clicked me " + count + " times");
 });
