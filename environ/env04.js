ecc1f602b259a59e9710ab2c08815f5f66ceba94
vmetallo-ctrl <vmetallo@ucsc.edu>
2026-05-15 08:31:48 -0700

    new

---
+++ environ/env04.js
@@ -0,0 +1,59 @@
+let environmentTitle = "The Forest Room";
+
+let environmentElements = ["trees", "dust", "wind", "dirt"];
+
+let mainEntity = {
+    name: "Logford",
+    type: "soft border",
+    isMoving: true,
+    favoriteElement: environmentElements[1]
+};
+
+
+let count = 0;
+let buttonCreature = {
+    name: "Logford's button",
+    species: "log",
+    favoriteFood: "unknown",
+    moods: ["Hi, I'm Logford! ", "Welcome to the Forest Page, isn't it pretty? ", "How are you doing today?", "Isn't this weather wonderfull?", "This is my favorite page", "Nice to meet you I'm Logford", "Whats your name?"]
+};
+
+$("#logford-button").click(function(){
+
+    count = count + 1;
+    console.log("the button is click");
+    let arrayPosition = count - 1;
+
+    let currentMood = buttonCreature.moods[arrayPosition];
+
+
+
+
+$("#Time").click(function(){
+    
+    console.log("the button is click");
+}
+    
+    //(count === 2) {
+   //     count = 0;
+   // }
+
+   // $("#Time").click(function(){
+  //  if (count === 1) {document.body.style.backgroundImage = "night.jpg";});}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//buttonCreature.moods = buttonCreature.moods + currentMood ;
+$("#logford-button").html(currentMood) ;
+
+});
